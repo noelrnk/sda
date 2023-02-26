4801924d75e0e2506d6b28841c5579679facda9b
noelrnk <noel.rinke@outlook.com>
2023-02-26 09:02:33 +0100

    Change image

--- src/app.py
+++ src/app.py
@@ -5,7 +5,8 @@ import data_preparation as dp
 import visualizations as vz
 
 
-st.image('img/fa_women_super_league.jpg')
+#st.image('img/fa_women_super_league.jpg')
+st.image('https://unsplash.com/photos/ARU18GpF6QQ')
 st.title("FA Women's Super League Analysis")
 st.text('Welcome to this Streamlit app! In this dashboard, the 2018/19 season of the Arsenal WFC and the Chelsea FCW '
         'is analysed.')
